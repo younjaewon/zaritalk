4e3301026819d1377e4e0004d13347597551aa11
younjaewon <97930034+younjaewon@users.noreply.github.com>
2023-02-20 13:36:53 +0900

    Feat : Add craco alias '@' with webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PATH from "./constants/path";
-import Lease from "./pages/Lease";
-import Layout from "./routes/Layout";
+import PATH from "@constants/path";
+import Layout from "@routes/Layout";
+import Lease from "@pages/Lease";
+
+import "./reset.css";
 
 function App() {
   return (
