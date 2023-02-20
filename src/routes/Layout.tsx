44782b919059562066bcac85303392b763b1d5b9
younjaewon <97930034+younjaewon@users.noreply.github.com>
2023-02-20 18:54:29 +0900

    Design: Layout Css

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,17 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 import { Outlet } from "react-router-dom";
+import { gray } from "@lib/styles/palette";
 
 type Props = {};
 
-// header, footer
+// header, main, footer
 const Layout = (props: Props) => {
   return (
-    <React.Fragment>
+    <Container>
       <main>
         <Outlet />
       </main>
-    </React.Fragment>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  height: 100vh;
+  background: ${gray[100]};
+`;
 
 export default Layout;
