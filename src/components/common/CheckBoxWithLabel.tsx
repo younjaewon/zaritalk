cf2063fa529e718cee1037febc214e488f07f8e7
younjaewon <97930034+younjaewon@users.noreply.github.com>
2023-02-20 22:41:59 +0900

    Design: isActive Props -> color, font-weight

--- src/components/common/CheckBoxWithLabel.tsx
+++ src/components/common/CheckBoxWithLabel.tsx
@@ -1,23 +1,28 @@
+import { blue } from "@lib/styles/palette";
 import React from "react";
 import styled from "styled-components";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   msg: string;
   id: string;
+  isActive: boolean;
 }
 
-const CheckBoxWithLabel = ({ msg, id, ...props }: Props) => {
+const CheckBoxWithLabel = ({ msg, id, isActive, ...props }: Props) => {
   return (
-    <Container>
+    <Container isActive={isActive}>
       <input type="checkbox" id={id} {...props} />
       <label htmlFor={id}>{msg}</label>
     </Container>
   );
 };
 
-const Container = styled.div`
+const Container = styled.div<{ isActive: boolean }>`
   display: flex;
   gap: 6px;
   align-items: center;
+  color: ${({ isActive }) => (isActive ? blue[100] : "black")};
+  font-weight: ${({ isActive }) => (isActive ? 700 : 400)};
 `;
+
 export default CheckBoxWithLabel;
