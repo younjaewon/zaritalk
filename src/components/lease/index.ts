bb10599f7758880506fe43a5d6d5dedc3d53d32b
younjaewon <97930034+younjaewon@users.noreply.github.com>
2023-02-20 21:21:02 +0900

    Rename : leaseType -> leaseTab

--- src/components/lease/index.ts
+++ src/components/lease/index.ts
@@ -1,5 +1,5 @@
 export { default as Information } from "./ExpenseInformation";
 
 export { default as Title } from "./Title";
-export { default as LeaseType } from "./LeaseType";
+export { default as LeaseTab } from "./LeaseTab";
 export { default as LeaseForm } from "./LeaseForm";
