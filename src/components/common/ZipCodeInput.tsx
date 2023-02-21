9d34917c51acc46c8e28a5f103c56a328d3d319f
younjaewon <97930034+younjaewon@users.noreply.github.com>
2023-02-21 11:47:39 +0900

    Feat : Add Kakao ZipCode Component

---
+++ src/components/common/ZipCodeInput.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useRef, useState } from "react";
+import DaumPostcodeEmbed from "react-daum-postcode";
+import styled from "styled-components";
+import { ReactComponent as SearchIcon } from "@lib/assets/svg/search.svg";
+import { black, gray } from "@lib/styles/palette";
+
+type Props = {};
+
+const ZipCodeInput = ({}: Props) => {
+  const [openPostcode, setOpenPostcode] = useState<boolean>(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event: any) => {
+    if (wrapperRef && !wrapperRef.current?.contains(event.target)) {
+      setOpenPostcode(false);
+    }
+  };
+
+  const handle = {
+    // 버튼 클릭 이벤트
+    clickButton: () => {
+      setOpenPostcode(true);
+    },
+
+    // 주소 선택 이벤트
+    selectAddress: (data: any) => {
+      console.log(`
+                주소: ${data.address},
+                우편번호: ${data.zonecode}
+            `);
+      setOpenPostcode(false);
+    },
+  };
+
+  return (
+    <Container ref={wrapperRef}>
+      <input type="text" placeholder="살고 계신 건물주소 또는 건물명을 입력하세요." />
+      <button onClick={handle.clickButton} className="openPoseBtn">
+        <SearchIcon />
+      </button>
+      {openPostcode && (
+        <ModalContainer>
+          <DaumPostcodeEmbed
+            onComplete={handle.selectAddress} // 값을 선택할 경우 실행되는 이벤트
+            autoClose={false} // 값을 선택할 경우 사용되는 DOM을 제거하여 자동 닫힘 설정
+            defaultQuery="판교역로 235" // 팝업을 열때 기본적으로 입력되는 검색어
+          />
+        </ModalContainer>
+      )}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  height: 46px;
+  margin: 8px 0;
+  padding: 11px 8px 11px 12px;
+  border: 1px solid ${gray[200]};
+  border-radius: 4px;
+  background-color: white;
+
+  input {
+    width: 100%;
+    color: ${black[100]};
+    line-height: 14px;
+
+    &::placeholder {
+      font-size: 14;
+      color: ${gray[400]};
+    }
+  }
+
+  .openPoseBtn {
+    background: none;
+    border: none;
+
+    svg {
+      width: 18px;
+      height: 18px;
+    }
+  }
+`;
+
+const ModalContainer = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50px;
+`;
+
+export default ZipCodeInput;
